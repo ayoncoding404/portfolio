d62c08aec3b294bf961597c25d028397d684d4de
Mashrur Ul Huq Ayon <113792024+ayoncoding404@users.noreply.github.com>
2023-05-25 18:09:34 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./Components/Header";
+import Nav from "./Components/Nav";
+import About from "./Components/About";
+import Experience from "./Components/Experience";
+import Services from "./Components/Services";
+import Portfolio from "./Components/Portfolio";
+import Testimonials from "./Components/Testimonials";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Testimonials.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Style/testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <div>Testimonials</div>
+  )
+}
+
+export default Testimonials
